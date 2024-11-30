bd09f8d50696fa9359048b4e080191813c67d7eb
rofazhasan <139000822+rofazhasan@users.noreply.github.com>
2024-11-30 10:19:01 +0600

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 from flask_socketio import SocketIO, emit
 from app.models import Class
 from datetime import datetime
+import pytz
 
 routes = Blueprint('routes', __name__)
 socketio = SocketIO(cors_allowed_origins="*")
@@ -15,16 +16,17 @@ def handle_connect():
     emit_classes()
 
 def emit_classes():
-    today = datetime.now().strftime('%A')
-    print(today)
-    classes = Class.query.filter_by(day=today).all()
-    print(classes)
-    now = datetime.now().time()
-    print(now)
+  local_tz=pytz.timezone('Asia/Dhaka')
+  today = datetime.now(local_tz).strftime('%A')
+  print(today)
+  classes = Class.query.filter_by(day=today).all()
+  print(classes)
+  now = datetime.now().time()
+  print(now)
 
-    ongoing_class = next((c for c in classes if c.start_time <= now < c.end_time), None)
+  ongoing_class = next((c for c in classes if c.start_time <= now < c.end_time), None)
 
-    data = {
+  data = {
         "today_classes": [{"subject": c.subject, "teacher": c.teacher, "room": c.room, "start_time": str(datetime.strptime(str(c.start_time) , "%H:%M:%S").strftime("%I:%M:%S %p")), "end_time": str(datetime.strptime(str(c.end_time) , "%H:%M:%S").strftime("%I:%M:%S %p"))} for c in classes],
         "current_class": {
             "subject": ongoing_class.subject,
@@ -35,4 +37,4 @@ def emit_classes():
         } if ongoing_class else {"message": "No ongoing class"}
     }
 
-    emit("class_data", data)
+  emit("class_data", data)
