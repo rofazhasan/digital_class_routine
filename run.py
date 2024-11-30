68219fd17d0e1dde320843880a74197f2f5f59f8
rofazhasan <139000822+rofazhasan@users.noreply.github.com>
2024-11-30 12:35:20 +0600

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,10 @@ from app import create_app
 from app.routes import socketio
 
 app = create_app()
+socketio = SocketIO(app, cors_allowed_origins="*")  # or specify the correct origin
+
+
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
