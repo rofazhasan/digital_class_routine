9ba1676f7db2b2af9e307d020570803b2f9f03f7
rofazhasan <139000822+rofazhasan@users.noreply.github.com>
2024-12-19 00:50:03 +0600

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,9 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+import os
 
 
 db = SQLAlchemy()
 
 def init_app(app):
-    app.config['SQLALCHEMY_DATABASE_URI']= "postgresql://classdb_ikhg_user:i0J5MvmIlZzQpVInUrNqeYPVNKUUEy1I@dpg-ct070rd6l47c7386hcb0-a.oregon-postgres.render.com/classdb_ikhg"
+    load_dotenv()  # Load environment variables from .env file
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     db.init_app(app)
